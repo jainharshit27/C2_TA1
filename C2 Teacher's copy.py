bb049dff00eaa3bed2c73ec9a9c64c8b4e759bd8
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-06-09 19:43:37 +0530

    Add files via upload

---
+++ C2 Teacher's copy.py
@@ -0,0 +1,31 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1200, 400))
+
+dino_rect = pygame.Rect(100, 250, 64, 64)
+cactus_rect = pygame.Rect(1100, 300, 32, 32)
+ground_rect = pygame.Rect(0, 330, 1200, 2)
+
+while True:
+    screen.fill((255, 255, 255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    cactus_rect.x = cactus_rect.x - 1
+    if cactus_rect.x <= -30:
+        cactus_rect.x = 1200
+        
+    pygame.draw.rect(screen, (100, 100, 100), dino_rect)
+    pygame.draw.rect(screen, (100, 100, 100), cactus_rect)
+    pygame.draw.rect(screen, (100, 100, 100), ground_rect)
+    
+    pygame.display.update()
+    
+    
+    
+    
+    
